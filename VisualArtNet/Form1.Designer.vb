193a68b16a7c4491fd3aea1762750204bcc9aa97
libertyernie <maybeway36@gmail.com>
2017-04-01 11:41:59 -0500

    Add detach button; block ad networks before they get to the main frame

--- VisualArtNet/Form1.Designer.vb
+++ VisualArtNet/Form1.Designer.vb
@@ -26,9 +26,13 @@ Partial Class Form1
         Me.WebBrowserF = New System.Windows.Forms.WebBrowser()
         Me.WebBrowserW = New System.Windows.Forms.WebBrowser()
         Me.WebBrowser1 = New System.Windows.Forms.WebBrowser()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
         Me.BrawlSplitter3 = New VisualArtNet.BrawlSplitter()
         Me.BrawlSplitter2 = New VisualArtNet.BrawlSplitter()
         Me.BrawlSplitter1 = New VisualArtNet.BrawlSplitter()
+        Me.Panel1.SuspendLayout()
         Me.SuspendLayout()
         '
         'WebBrowserD
@@ -37,7 +41,7 @@ Partial Class Form1
         Me.WebBrowserD.Location = New System.Drawing.Point(0, 0)
         Me.WebBrowserD.MinimumSize = New System.Drawing.Size(20, 20)
         Me.WebBrowserD.Name = "WebBrowserD"
-        Me.WebBrowserD.Size = New System.Drawing.Size(657, 250)
+        Me.WebBrowserD.Size = New System.Drawing.Size(784, 250)
         Me.WebBrowserD.TabIndex = 0
         '
         'WebBrowserF
@@ -46,34 +50,64 @@ Partial Class Form1
         Me.WebBrowserF.Location = New System.Drawing.Point(0, 259)
         Me.WebBrowserF.MinimumSize = New System.Drawing.Size(20, 20)
         Me.WebBrowserF.Name = "WebBrowserF"
-        Me.WebBrowserF.Size = New System.Drawing.Size(300, 329)
+        Me.WebBrowserF.Size = New System.Drawing.Size(300, 302)
         Me.WebBrowserF.TabIndex = 1
         '
         'WebBrowserW
         '
         Me.WebBrowserW.Dock = System.Windows.Forms.DockStyle.Right
-        Me.WebBrowserW.Location = New System.Drawing.Point(357, 259)
+        Me.WebBrowserW.Location = New System.Drawing.Point(484, 259)
         Me.WebBrowserW.MinimumSize = New System.Drawing.Size(20, 20)
         Me.WebBrowserW.Name = "WebBrowserW"
-        Me.WebBrowserW.Size = New System.Drawing.Size(300, 329)
+        Me.WebBrowserW.Size = New System.Drawing.Size(300, 302)
         Me.WebBrowserW.TabIndex = 2
         '
         'WebBrowser1
         '
         Me.WebBrowser1.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.WebBrowser1.Location = New System.Drawing.Point(309, 259)
+        Me.WebBrowser1.Location = New System.Drawing.Point(309, 283)
         Me.WebBrowser1.MinimumSize = New System.Drawing.Size(20, 20)
         Me.WebBrowser1.Name = "WebBrowser1"
-        Me.WebBrowser1.Size = New System.Drawing.Size(39, 329)
+        Me.WebBrowser1.Size = New System.Drawing.Size(166, 278)
         Me.WebBrowser1.TabIndex = 3
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.TextBox1)
+        Me.Panel1.Controls.Add(Me.Button1)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Panel1.Location = New System.Drawing.Point(309, 259)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(166, 24)
+        Me.Panel1.TabIndex = 7
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TextBox1.Location = New System.Drawing.Point(0, 0)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ReadOnly = True
+        Me.TextBox1.Size = New System.Drawing.Size(91, 24)
+        Me.TextBox1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Dock = System.Windows.Forms.DockStyle.Right
+        Me.Button1.Location = New System.Drawing.Point(91, 0)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 24)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Detach"
+        Me.Button1.UseVisualStyleBackColor = True
         '
         'BrawlSplitter3
         '
         Me.BrawlSplitter3.ControlToHide = Me.WebBrowserW
         Me.BrawlSplitter3.Dock = System.Windows.Forms.DockStyle.Right
-        Me.BrawlSplitter3.Location = New System.Drawing.Point(348, 259)
+        Me.BrawlSplitter3.Location = New System.Drawing.Point(475, 259)
         Me.BrawlSplitter3.Name = "BrawlSplitter3"
-        Me.BrawlSplitter3.Size = New System.Drawing.Size(9, 329)
+        Me.BrawlSplitter3.Size = New System.Drawing.Size(9, 302)
         Me.BrawlSplitter3.TabIndex = 6
         Me.BrawlSplitter3.TabStop = False
         '
@@ -82,7 +116,7 @@ Partial Class Form1
         Me.BrawlSplitter2.ControlToHide = Me.WebBrowserF
         Me.BrawlSplitter2.Location = New System.Drawing.Point(300, 259)
         Me.BrawlSplitter2.Name = "BrawlSplitter2"
-        Me.BrawlSplitter2.Size = New System.Drawing.Size(9, 329)
+        Me.BrawlSplitter2.Size = New System.Drawing.Size(9, 302)
         Me.BrawlSplitter2.TabIndex = 5
         Me.BrawlSplitter2.TabStop = False
         '
@@ -92,7 +126,7 @@ Partial Class Form1
         Me.BrawlSplitter1.Dock = System.Windows.Forms.DockStyle.Top
         Me.BrawlSplitter1.Location = New System.Drawing.Point(0, 250)
         Me.BrawlSplitter1.Name = "BrawlSplitter1"
-        Me.BrawlSplitter1.Size = New System.Drawing.Size(657, 9)
+        Me.BrawlSplitter1.Size = New System.Drawing.Size(784, 9)
         Me.BrawlSplitter1.TabIndex = 4
         Me.BrawlSplitter1.TabStop = False
         '
@@ -100,8 +134,9 @@ Partial Class Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(657, 588)
+        Me.ClientSize = New System.Drawing.Size(784, 561)
         Me.Controls.Add(Me.WebBrowser1)
+        Me.Controls.Add(Me.Panel1)
         Me.Controls.Add(Me.BrawlSplitter3)
         Me.Controls.Add(Me.WebBrowserW)
         Me.Controls.Add(Me.BrawlSplitter2)
@@ -110,6 +145,8 @@ Partial Class Form1
         Me.Controls.Add(Me.WebBrowserD)
         Me.Name = "Form1"
         Me.Text = "Visual Art.NET"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
         Me.ResumeLayout(False)
 
     End Sub
@@ -121,4 +158,7 @@ Partial Class Form1
     Friend WithEvents BrawlSplitter1 As BrawlSplitter
     Friend WithEvents BrawlSplitter2 As BrawlSplitter
     Friend WithEvents BrawlSplitter3 As BrawlSplitter
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
 End Class
--- VisualArtNet/Form1.vb
+++ VisualArtNet/Form1.vb
@@ -8,27 +8,48 @@ Public Class Form1
     End Sub
 
     Private Sub WebBrowserD_Navigating(sender As Object, e As WebBrowserNavigatingEventArgs) Handles WebBrowserD.Navigating
-        If Not e.Url.AbsolutePath = "/notifications/" Then
-            e.Cancel = True
-            WebBrowser1.Url = e.Url
+        If String.IsNullOrEmpty(e.TargetFrameName) Then
+            If Not e.Url.AbsolutePath = "/notifications/" Then
+                e.Cancel = True
+                WebBrowser1.Url = e.Url
+            End If
         End If
     End Sub
 
     Private Sub WebBrowserF_Navigating(sender As Object, e As WebBrowserNavigatingEventArgs) Handles WebBrowserF.Navigating
-        If Not e.Url.AbsolutePath.StartsWith("/msg") Then
+        If e.Url.Host.Contains("betrad.com") Or e.Url.Host.Contains("doubleclick.net") Then
             e.Cancel = True
-            WebBrowser1.Url = e.Url
+            Return
+        End If
+        If String.IsNullOrEmpty(e.TargetFrameName) Then
+            If Not e.Url.AbsolutePath.StartsWith("/msg") Then
+                e.Cancel = True
+                WebBrowser1.Url = e.Url
+            End If
         End If
     End Sub
 
     Private Sub WebBrowserW_Navigating(sender As Object, e As WebBrowserNavigatingEventArgs) Handles WebBrowserW.Navigating
-        If e.Url.AbsolutePath.StartsWith("/~") Then
-            e.Cancel = True
-            WebBrowser1.Url = e.Url
+        If String.IsNullOrEmpty(e.TargetFrameName) Then
+            If e.Url.AbsolutePath.StartsWith("/~") Then
+                e.Cancel = True
+                WebBrowser1.Url = e.Url
+            End If
         End If
     End Sub
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         WebBrowserHelper.FixBrowserVersion()
+    End Sub
+
+    Private Sub WebBrowser1_Navigating(sender As Object, e As WebBrowserNavigatingEventArgs) Handles WebBrowser1.Navigating
+        If String.IsNullOrEmpty(e.TargetFrameName) Then
+            TextBox1.Text = e.Url.AbsoluteUri
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Process.Start(WebBrowser1.Url.AbsoluteUri)
+        WebBrowser1.Url = New Uri("about:blank")
     End Sub
 End Class
